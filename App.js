e5a1559006dd29ed90e9e2e88b2d407a786b1fb1
webbank <webbank@Dulguuns-MacBook-Pro.local>
2019-10-31 03:00:16 +0800

    Add required packages and add tab navigator

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import Portfolio from "./screens/Portfolio";
+import Settings from "./screens/Settings";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const TabNavigator = createBottomTabNavigator({
+  Portfolio: Portfolio,
+  Settings: Settings
 });
+
+const AppContainer = createAppContainer(TabNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
